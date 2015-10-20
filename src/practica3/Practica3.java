606f80b265eca676af736ed5f13b02d3598eddbb
usuario <usuario@10.22.143.11>
2015-10-20 10:14:11 +0100

    Segundo parte del Histogram

--- src/practica3/Practica3.java
+++ src/practica3/Practica3.java
@@ -3,8 +3,14 @@ package practica3;
 public class Practica3 {
 
     public static void main(String[] args) {
-        HistogramDisplay histogram = new HistogramDisplay();
-        histogram.execute();
+        Histogram <String> histogram = new Histogram<>();
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail..com");
+        histogram.increment("ull.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");
+        HistogramDisplay histograma = new HistogramDisplay(histogram);
+        histograma.execute();
         
     }
     
