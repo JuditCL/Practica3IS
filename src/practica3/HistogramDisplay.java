a3551447f02a16d7bd784cbcf77ebeda2e9d7146
usuario <usuario@10.22.143.11>
2015-10-20 09:25:48 +0100

    Primera parte del Histogram

---
+++ src/practica3/HistogramDisplay.java
@@ -0,0 +1,44 @@
+package practica3;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class HistogramDisplay extends ApplicationFrame{
+
+    public HistogramDisplay() {
+        super("Histogram");
+        setContentPane(createPanel());
+        pack();
+    }
+
+    private JPanel createPanel() {
+        ChartPanel chartPanel = new ChartPanel (createChart(createDataset()));
+        chartPanel.setPreferredSize(new Dimension(500,500));
+        return chartPanel;
+    }
+    
+    private JFreeChart createChart(DefaultCategoryDataset dataSet){
+        JFreeChart chart = ChartFactory.createBarChart("Histogram", "Domains", "N emails", dataSet, PlotOrientation.VERTICAL, false, false, false);
+        return chart;
+    }
+    private DefaultCategoryDataset createDataset(){
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        dataSet.addValue(10, "", "gmail.com");
+        dataSet.addValue(24, "", "hotmail.com");
+        dataSet.addValue(15, "", "ull.es");
+        dataSet.addValue(30, "", "ulpgc.es");
+        return dataSet;
+    }
+
+    void execute() {
+        setVisible(true);
+    }
+}
