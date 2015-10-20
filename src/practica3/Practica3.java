278cfc27f1fc1bf2ee6c7208deacfd458b9e96d0
usuario <usuario@10.22.143.11>
2015-10-20 09:25:33 +0100

    Primera parte del Histogram

---
+++ src/practica3/Practica3.java
@@ -0,0 +1,11 @@
+package practica3;
+
+public class Practica3 {
+
+    public static void main(String[] args) {
+        HistogramDisplay histogram = new HistogramDisplay();
+        histogram.execute();
+        
+    }
+    
+}
